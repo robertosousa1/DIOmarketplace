06a90af7483f6d8f909f9119a2f2ac938b4756f0
Roberto <rm77831@fiap.com.br>
2020-07-10 21:16:26 -0300

    Aula 4 - Configura StatusBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <Routes />
+  </>
+);
 
 export default App;
