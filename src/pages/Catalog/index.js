e916430e3f0cca206e2774259e66948b1fea13c2
Roberto <rm77831@fiap.com.br>
2020-07-11 14:53:33 -0300

    Aula 7 - Cria página de catálogo

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,6 +1,66 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export default function App() {
-  return <Container />;
+import { View } from 'react-native';
+
+import formatValue from '../../utils/formatValue';
+
+import {
+  Container,
+  ProductContainer,
+  ProductImage,
+  ProductList,
+  Product,
+  ProductTitle,
+  PriceContainer,
+  ProductPrice,
+  ProductButton,
+  ProductButtonText,
+} from './styles';
+
+export default function Catalog() {
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      title: 'Assinatura Trimestral',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594489429/dio/quarterly_subscription_jtdoae.png',
+      price: 150,
+    },
+    {
+      id: '2',
+      title: 'Assinatura Anual',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594489430/dio/annual_subscription_ziy5b7.png',
+      price: 540,
+    },
+  ]);
+
+  return (
+    <Container>
+      <ProductContainer>
+        <ProductList
+          data={products}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={<View />}
+          ListFooterComponentStyle={{
+            height: 80,
+          }}
+          renderItem={({ item }) => (
+            <Product>
+              <ProductImage source={{ uri: item.image_url }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <PriceContainer>
+                <ProductPrice>{formatValue(item.price)}</ProductPrice>
+                <ProductButton onPress={() => {}}>
+                  <ProductButtonText>adicionar</ProductButtonText>
+                  <FeatherIcon size={30} name="plus-circle" color="#d1d7e9" />
+                </ProductButton>
+              </PriceContainer>
+            </Product>
+          )}
+        />
+      </ProductContainer>
+    </Container>
+  );
 }
--- src/pages/Catalog/styles.js
+++ src/pages/Catalog/styles.js
@@ -1,7 +1,76 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
-  padding: 30px;
-  background: #333;
+  align-items: center;
+`;
+
+export const ProductContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  margin-top: 40px;
+  border-radius: 5px;
+`;
+
+export const ProductList = styled(FlatList).attrs({
+  numColumns: 1,
+})`
+  flex: 1;
+  padding: 40px 20px;
+`;
+
+export const Product = styled.View`
+  flex: 1;
+  margin: 8px;
+  background: #626878;
+  padding: 32px 12px;
+  border-radius: 25px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 90%;
+  height: 220px;
+  align-self: center;
+  box-shadow: 4px 12px 10px #312e38;
+`;
+
+export const ProductTitle = styled.Text`
+  color: #b9c0d2;
+  font-size: 22px;
+  margin-top: 10px;
+  font-weight: 400;
+  line-height: 40px;
+  letter-spacing: 3.5px;
+  text-align: center;
+`;
+
+export const PriceContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: auto;
+  padding: 24px 32px 4px;
+`;
+
+export const ProductPrice = styled.Text`
+  color: #f3f9ff;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+export const ProductButton = styled.TouchableOpacity`
+  background: #4f5564;
+  padding: 4px 8px;
+  border-radius: 100px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const ProductButtonText = styled.Text`
+  color: #b9c0d2;
+  font-weight: 800;
+  padding: 0 4px;
+  font-size: 12px;
+  text-transform: uppercase;
 `;
