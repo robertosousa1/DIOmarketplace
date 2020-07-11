ed43c83e3d4578dddd44a27f41e3807ca0937f54
Roberto <rm77831@fiap.com.br>
2020-07-10 21:28:36 -0300

    Aula 5 - Estiliza com Styled Components

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
-  },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import { Container } from './styles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Catalog</Text>
-    </View>
-  );
+  return <Container />;
 }
