e59054b81612309ecff6cbc40aa60791d014d25a
Roberto <rm77831@fiap.com.br>
2020-07-11 16:00:48 -0300

    Aula 8 - Cria página do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,98 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export default function App() {
-  return <Container />;
+// import EmptyCart from '../../components/EmptyCart';
+
+import {
+  Container,
+  ProductContainer,
+  ProductList,
+  Product,
+  ProductImage,
+  ProductTitleContainer,
+  ProductTitle,
+  ProductPriceContainer,
+  ProductSinglePrice,
+  TotalContainer,
+  ProductPrice,
+  ProductQuantity,
+  ActionContainer,
+  ActionButton,
+  TotalProductsContainer,
+  TotalProductsText,
+  SubtotalValue,
+} from './styles';
+
+import formatValue from '../../utils/formatValue';
+
+export default function Cart() {
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      title: 'Assinatura Trimestral',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
+      quantity: 1,
+      price: 150,
+    },
+    {
+      id: '2',
+      title: 'Assinatura Anual',
+      image_url:
+        'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/annual_subscription_qyolci.png',
+      quantity: 1,
+      price: 540,
+    },
+  ]);
+
+  return (
+    <Container>
+      <ProductContainer>
+        <ProductList
+          data={products}
+          keyExtractor={(item) => item.id}
+          // ListEmptyComponent={<EmptyCart />}
+          ListFooterComponent={<View />}
+          ListFooterComponentStyle={{
+            height: 80,
+          }}
+          renderItem={({ item }) => (
+            <Product>
+              <ProductImage source={{ uri: item.image_url }} />
+              <ProductTitleContainer>
+                <ProductTitle>{item.title}</ProductTitle>
+                <ProductPriceContainer>
+                  <ProductSinglePrice>
+                    {formatValue(item.price)}
+                  </ProductSinglePrice>
+
+                  <TotalContainer>
+                    <ProductQuantity>{`${item.quantity}x`}</ProductQuantity>
+
+                    <ProductPrice>
+                      {formatValue(item.price * item.quantity)}
+                    </ProductPrice>
+                  </TotalContainer>
+                </ProductPriceContainer>
+              </ProductTitleContainer>
+              <ActionContainer>
+                <ActionButton onPress={() => {}}>
+                  <FeatherIcon name="plus" color="#E83F5B" size={16} />
+                </ActionButton>
+                <ActionButton onPress={() => {}}>
+                  <FeatherIcon name="minus" color="#E83F5B" size={16} />
+                </ActionButton>
+              </ActionContainer>
+            </Product>
+          )}
+        />
+      </ProductContainer>
+      <TotalProductsContainer>
+        <FeatherIcon name="shopping-cart" color="#fff" size={24} />
+        <TotalProductsText>2 itens</TotalProductsText>
+        <SubtotalValue>R$100,00</SubtotalValue>
+      </TotalProductsContainer>
+    </Container>
+  );
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,7 +1,115 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
-  padding: 30px;
-  background: #333;
+  align-items: center;
+`;
+
+export const ProductContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  margin-top: 60px;
+  border-radius: 20px;
+  background-color: #e83f5b;
+`;
+
+export const ProductList = styled(FlatList)`
+  flex: 1;
+  padding: 30px 10px;
+`;
+
+export const Product = styled.View`
+  flex: 1;
+  flex-direction: row;
+  margin: 5px;
+  background: #fff;
+  min-width: 350px;
+  min-height: 130px;
+  padding: 15px 10px;
+  border-radius: 20px;
+`;
+
+export const ProductImage = styled.Image`
+  height: 92px;
+  width: 140px;
+`;
+
+export const ProductTitleContainer = styled.View`
+  font-size: 16px;
+  margin-left: 5px;
+  margin-top: 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+`;
+
+export const ProductPriceContainer = styled.View`
+  flex-direction: column;
+`;
+
+export const TotalContainer = styled.View`
+  flex-direction: row;
+  margin-top: 8px;
+`;
+
+export const ProductSinglePrice = styled.Text`
+  font-size: 12px;
+  color: #414756;
+  margin-top: 8px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-weight: bold;
+  margin-top: 5px;
+  font-size: 16px;
+  color: #e83f5b;
+`;
+
+export const ProductQuantity = styled.Text`
+  font-weight: bold;
+  margin-top: 5px;
+  margin-right: 10px;
+  font-size: 16px;
+  color: #e83f5b;
+`;
+
+export const ActionContainer = styled.View`
+  align-self: flex-end;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: auto;
+`;
+
+export const ActionButton = styled.TouchableOpacity`
+  background: rgba(232, 63, 91, 0.1);
+  border-radius: 5px;
+  padding: 12px;
+  margin-bottom: 5px;
+`;
+
+export const TotalProductsContainer = styled.View`
+  bottom: 0px;
+  position: absolute;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 36px 40px;
+  border-radius: 10px;
+  background: #e83f5b;
+`;
+
+export const TotalProductsText = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  margin-left: 15px;
+  flex: 1;
+  font-weight: bold;
+`;
+
+export const SubtotalValue = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
 `;
